3f44268659ba0fb56b5e7c07a01a517d0a62fc5e
randytalla <randytala2@gmail.com>
2025-03-27 06:24:20 -0700

    Added the product model in my models.py

--- ecommerce_api/products/models.py
+++ ecommerce_api/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    stock_quantity = models.PositiveIntegerField()
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
