d884d137a585a786cd1ca71c6196c844bc73c8f5
randytalla <randytala2@gmail.com>
2025-03-27 06:13:45 -0700

    ran migratins to save changes in our model and our setting.py file

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'orders',
     'payments',
     'rest_framework',
+    'rest_framework_simplejwt', 
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +129,26 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+
+
+AUTHENTICATION_BACKENDS = [
+    'users.authentication.EmailAuthBackend',  # Custom backend
+    'django.contrib.auth.backends.ModelBackend',  # Default backend
+]
 
 
 
