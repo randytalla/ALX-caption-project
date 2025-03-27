35b5b94e23fc80912961274e30e3dd24f158cd83
randytalla <randytala2@gmail.com>
2025-03-27 06:37:52 -0700

    created a viewset that will handle all CRUD operations

--- ecommerce_api/products/views.py
+++ ecommerce_api/products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing products.
+    - GET (list, retrieve)
+    - POST (create)
+    - PUT/PATCH (update)
+    - DELETE (delete)
+    """
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow anyone to view, but only authenticated users to edit
