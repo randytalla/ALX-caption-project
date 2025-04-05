aca26b3151e123687e21821718f0e79f88d30d13
randytalla <randytala2@gmail.com>
2025-04-05 02:52:21 -0700

    eddited my setting.py for the error i had for the pagination, because i did not set it up well in the setting.py file

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -113,8 +113,9 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'PAGE_SIZE': 5
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 SIMPLE_JWT = {
